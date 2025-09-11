4b2733ed376b976431bbbd858520584ab786aa97
Akshay Nile <akshaynile8849@gmail.com>
2025-09-11 13:48:11 +0530

    fixed service-worker not detecting available updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,37 @@
-const CACHE_KEY = "baby-monitor-cache";
+const APP_CACHE = "baby-monitor-app-cache";
+const META_CACHE = "baby-monitor-meta-cache";
 const PUBLIC_FILES = ["./index.html", "./manifest.json", "./favicon.png"];
 
-async function isIndexHtmlUpdated() {
+async function storeOrLoadMetaCache(key, value) {
     try {
-        const cache = await caches.open(CACHE_KEY);
-        const cachedResponse = await cache.match("./index.html");
-        if (!cachedResponse) return true;
-        const cachedHtml = (await cachedResponse.text()).trim();
+        const cache = await caches.open(META_CACHE);
+        const request = new Request(`https://local.meta.cache/${key}`);
 
-        const serverResponse = await fetch("./index.html", { cache: "no-store" });
-        const serverHtml = (await serverResponse.text()).trim();
+        if (value === undefined) {
+            const response = await cache.match(request);
+            return response ? await response.json() : null;
+        } else {
+            const response = new Response(JSON.stringify(value));
+            await cache.put(request, response);
+        }
+    } catch (err) {
+        console.error("Error While Dealing With Meta-Cache:", err);
+        return false;
+    }
+}
 
-        return serverHtml !== cachedHtml;
+async function isUpdateAvailable() {
+    try {
+        const lastUpdateKey = "last-update";
+        const lastUpdateVal = await storeOrLoadMetaCache(lastUpdateKey) ?? "0";
+
+        const response = await fetch("https://akshaynile.pythonanywhere.com/baby-monitor", {
+            headers: { "X-Last-Update": lastUpdateVal }, cache: "no-store"
+        });
+        const data = await response.json();
+
+        if (data.is_updated) await storeOrLoadMetaCache(lastUpdateKey, data.last_update);
+        return data.is_updated;
     } catch (err) {
         console.error("Error While Checking For Update:", err);
         return false;
@@ -20,23 +40,24 @@ async function isIndexHtmlUpdated() {
 
 self.addEventListener("activate", (event) => {
     event.waitUntil((async () => {
-        if (await isIndexHtmlUpdated()) {
+        if (await isUpdateAvailable()) {
             console.warn("New Update Detected!\nReplacing the old cache with new one");
-            await caches.delete(CACHE_KEY);
+            await caches.delete(APP_CACHE);
 
-            const cache = await caches.open(CACHE_KEY);
+            const cache = await caches.open(APP_CACHE);
             await cache.addAll(PUBLIC_FILES);
         }
+        await self.clients.claim();
     })());
 });
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(caches.open(CACHE_KEY)
+    event.waitUntil(caches.open(APP_CACHE)
         .then((cache) => cache.addAll(PUBLIC_FILES)));
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(caches.open(CACHE_KEY).then(async (cache) => {
+    event.respondWith(caches.open(APP_CACHE).then(async (cache) => {
         const cached = await cache.match(event.request);
         if (cached) return cached;
 
