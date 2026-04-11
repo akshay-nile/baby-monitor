c4991cdad69eac2aef09eaa672895c118343e875
Akshay Nile <akshaynile8849@gmail.com>
2026-04-11 23:26:45 +0530

    removed the service-worker.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
+import { PrimeReactProvider } from 'primereact/api';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { PrimeReactProvider } from 'primereact/api';
-
-import './index.css';
 import ToastContextProvider from './contexts/ToastMessage/ToastContextProvider.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,22 +14,3 @@ createRoot(document.getElementById('root')!).render(
     </PrimeReactProvider>
   </StrictMode>,
 );
-
-if ('serviceWorker' in navigator && window.location.href.startsWith('https')) {
-  window.addEventListener('load', async () => {
-    try {
-      const registration = await navigator.serviceWorker.register('./service-worker.js');
-      await navigator.serviceWorker.ready;
-      console.log('Service Worker Registered!');
-
-      navigator.serviceWorker.addEventListener('message', (event) => {
-        if (event.data === 'UPDATED') window.location.reload();
-        console.log('Message From SW:', event.data);
-      });
-
-      const controller = navigator.serviceWorker.controller ?? registration.active;
-      controller?.postMessage('CHECK-UPDATE');
-    }
-    catch (err) { console.error('Service Worker Error:', err); }
-  });
-}
